c087990ca498d81a2ac202e04d8e8bdfc34b2a22
viaan <viaan114@gmail.com>
2026-05-15 21:42:25 +0530

    first commit

---
+++ act1.py
@@ -0,0 +1,7 @@
+print("welcome to codingal")
+
+print("my name is viaan")
+
+print("i am here to learn coding")
+
+print("the sum of 5 and 3 is", 5 + 3)
---
+++ act2.py
@@ -0,0 +1,9 @@
+print("welcome to the world of python")
+
+print(4)
+
+print("python/n")
+
+print("the main keywords of python are:")
+import keyword
+print(keyword.kwlist)
