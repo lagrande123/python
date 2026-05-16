88d667a2264ba133d740c754efc4130734056447
viaan <viaan114@gmail.com>
2026-05-16 21:19:38 +0530

    updates

---
+++ lesson2/act1.py
@@ -0,0 +1,18 @@
+name="codingal"
+age=13
+isstudent= True
+weight= 39
+
+print("name:",name )
+
+print("datatype of name is", type(name))
+
+
+print("age:",age   )
+print("datatype of age is", type(age))
+
+print("is student ? :", isstudent)
+print("datatype of isstudent is", type(isstudent))
+
+print("weight:", weight)
+print("datatype of weight is", type(weight))
